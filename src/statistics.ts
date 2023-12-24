55a60e85ee7b0e66d472d62c9e21d5c72d11d7fb
VMois <vmois@protonmail.com>
2023-12-24 23:02:17 +0100

    Add "last" query param that allows to query smaller interval of stats
    - provided predefined values only, return 7 days if nothing is provided or input is not correct
    - for simplicity, no regex, no external validation library.
    
    closes #8

--- src/statistics.ts
+++ src/statistics.ts
@@ -2,7 +2,28 @@ import Storage from './storage'
 import Env from './env'
 
 
-const Statistics = async (_: Request, env: Env): Promise<Response> => {
+function convertDurationToMinutes(duration: string | null): number {
+	const day = 24 * 60;
+	const month = 30 * day;
+
+  switch (duration) {
+		case '7d':
+			return 7 * day;
+		case '30d':
+			return 30 * day;
+		case '3m':
+			return 3 * month;
+		case '6m':
+			return 6 * month;
+		case '1y':
+			return 12 * month;
+		default:
+			return 7 * day;
+  }
+}
+
+
+const Statistics = async (req: Request, env: Env): Promise<Response> => {
     const headers = {
         'Content-type': 'application/json',
     };
@@ -10,7 +31,11 @@ const Statistics = async (_: Request, env: Env): Promise<Response> => {
     const storage = new Storage();
     await storage.init(env);
 
-    const avgViewTime = await storage.getAvgViewTimePerPage();
+		const url = new URL(req.url);
+		const last = url.searchParams.get('last');
+		const minutes = convertDurationToMinutes(last);
+
+    const avgViewTime = await storage.getAvgViewTimePerPage(minutes);
     return new Response(JSON.stringify(avgViewTime), { headers });
 }
 
